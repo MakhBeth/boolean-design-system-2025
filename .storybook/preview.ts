cc4fc34f676810495291c39515028a497ed6a658
Davide Di Pumpo <davide.dipumpo@tot.money>
2024-12-03 15:45:28 +0100

    Adds first story, Badge

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,16 @@
 /** @type { import('@storybook/react').Preview } */
+
+import "../src/styles/reset.css";
+
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
